3d80e3e41029f596297bc0b864e18fdf1491e4bc
Majid Ali <majidaliofficial129@gmail.com>
2026-04-15 23:12:22 -0700

    add EditDashboardPage component with initial layout

---
+++ src/app/dashboard/[id]/edit/page.tsx
@@ -0,0 +1,8 @@
+export default function EditDashboardPage() {
+  return (
+    <div>
+      <h1>Edit Dashboard</h1>
+      {/* Add your edit dashboard form or components here */}
+    </div>
+  );
+}
